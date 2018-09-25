bf70bee93adb0561fe74be8b1c78c21e738d05d7
Orhan Sönmez <orhnsnmz@gmail.com>
2018-09-25 16:07:58 +0200

    Fix zIndex values

--- src/SortableList.js
+++ src/SortableList.js
@@ -12,7 +12,7 @@ import { shallowEqual, swapArrayElements } from "./utils";
 import Row from "./Row";
 
 const AUTOSCROLL_INTERVAL = 100;
-const ZINDEX = Platform.OS === 'ios' ? 'zIndex' : 'elevation';
+const ZINDEX = "zIndex";
 
 function uniqueRowKey(key) {
   return `${key}${uniqueRowKey.id}`;
@@ -269,7 +269,7 @@ export default class SortableList extends Component {
     let nextY = 0;
 
     return order.map((key, index) => {
-      const style = { [ZINDEX]: 0 };
+      const style = { [ZINDEX]: index };
       const location = { x: 0, y: 0 };
 
       if (rowsLayouts) {
@@ -286,7 +286,7 @@ export default class SortableList extends Component {
       const released = releasedRowKey === key;
 
       if (active || released) {
-        style[ZINDEX] = 100;
+        style[ZINDEX] = order.length;
       }
 
       return (
